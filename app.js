95e859faf4ca370a2dc0adc969bb784bf30773fb
ryansummerlin <ryansummerlin777@gmail.com>
2023-06-08 19:31:41 -0700

    learned how to use jwt

--- app.js
+++ app.js
@@ -3,14 +3,18 @@ require('dotenv').config();
 
 // Import package
 
-// Your code here
+const jwt = require('jsonwebtoken');
 
 // Define variables - DO NOT MODIFY
 
 // 1. Sign (create) a JWT containing your email address
 let token; // DO NOT MODIFY! Re-assign the token variable below.
 
-// Your code here
+token = jwt.sign(
+    { email: 'ryansummerlin777@gmail.com' },
+    process.env.SECRET_KEY,
+    { expiresIn: '1s' }
+);
 
 // See the JWT in the console - DO NOT MODIFY
 console.log('JWT:', token);
@@ -19,7 +23,7 @@ console.log('JWT:', token);
 
 let payload; // DO NOT MODIFY! Re-assign the payload variable below.
 
-// Your code here
+payload = jwt.decode(token);
 
 // See the decoded payload in the console - DO NOT MODIFY
 console.log('Payload:', payload);
@@ -28,17 +32,24 @@ console.log('Payload:', payload);
 
 let verifiedPayload; // DO NOT MODIFY! Re-assign the verifiedPayload variable below.
 
-// Your code here
+// verifiedPayload = jwt.verify(token, process.env.SECRET_KEY);
 
 // See the verified payload in the console - DO NOT MODIFY
-console.log('Verified Payload:', verifiedPayload);
+// console.log('Verified Payload:', verifiedPayload);
 
 // (Optional) Bonus: Catch Error With Invalid Signature
 // Generate an alternate secret key and use it
 //    To "try" to get the payload using jwt.verify
 //    Then "catch" the error and log it to the console.
 
-// Your code here
+// let otherKey = require('crypto').randomBytes(64).toString('hex');
+
+// try {
+//     verifiedPayload = jwt.verify(token, otherKey);
+// } catch(e) {
+//     console.log(e.message);
+// }
+
 
 // (Optional) Bonus: Catch Error With Expired Token
 // First, set the token's expiration (above) to 1 second
@@ -46,4 +57,10 @@ console.log('Verified Payload:', verifiedPayload);
 //    To "try" to get the payload using jwt.verify (with proper secret)
 //    Then "catch" the error and log it to the console
 
-// Your code here
+try {
+    setTimeout(() => {
+        jwt.verify(token, process.env.SECRET_KEY);
+    }, 2000)
+} catch(e) {
+    console.log(e.message);
+}
